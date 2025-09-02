9b94f6cc35ab7c17b7f0af8dcc2a78e5469d927d
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-02 10:53:41 -0400

    Build methods to pass tests

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class BankAccount
-  def deposit(amount); end
-  def withdraw(amount); end
-  def balance; end
+  attr_accessor :balance 
+  
+  def initialize 
+    @balance = 0
+  end
+  
+  def deposit(amount)
+    @balance += amount 
+  end
+
+  def withdraw(amount) 
+    @balance -= amount 
+  end
+
+  def balance
+    @balance 
+  end
 end
