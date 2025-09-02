434315f5a0d0e5597ddfcc6f966b2b4c5b8d96df
Kyle <kylegranthousel@gmail.com>
2025-09-02 11:42:50 -0400

    Add specs for bankaccount

--- spec/student/bank_account_spec.rb
+++ spec/student/bank_account_spec.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 describe BankAccount do
-  # ...student specs...
+  it "deposits money" do
+    account = BankAccount.new(100)
+    expect(account.deposit(100)).to eq(200)
+  end
+
+  it "withdraws money" do
+    account = BankAccount.new(1000)
+    expect(account.withdraw(100)).to eq(900)
+  end
+
+  it "displays the user's balance" do
+    account = BankAccount.new(500)
+    expect(account.balance).to eq(500)
+  end
 end
