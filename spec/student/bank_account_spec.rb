de79da82b11a27d42429650a1363827a5e66f2ea
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-02 10:53:27 -0400

    Fix syntax errors

--- spec/student/bank_account_spec.rb
+++ spec/student/bank_account_spec.rb
@@ -2,19 +2,19 @@
 
 describe BankAccount do
   it "returns the account balance" do 
-    expect(BankAccount.new.balance.to eq(0))
+    expect(BankAccount.new.balance).to eq(0)
   end
   
   it "deposits money into the account" do 
     bank_account = BankAccount.new
     bank_account.deposit(100)
-    expect(bank_account.balance.to eq(100))
+    expect(bank_account.balance).to eq(100)
   end 
 
   it "withdraws money from the account" do 
-    bank_account = BankAccount.new(0)
+    bank_account = BankAccount.new
     bank_account.deposit(100)
     bank_account.withdraw(20)
-    expect(bank_account.balance.to eq(80))
+    expect(bank_account.balance).to eq(80)
   end
 end
