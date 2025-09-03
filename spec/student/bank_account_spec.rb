a0ebfdd2d5a310351243370e0cce1f50ae771095
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-03 09:07:23 -0400

    Refactor with subject

--- spec/student/bank_account_spec.rb
+++ spec/student/bank_account_spec.rb
@@ -1,19 +1,17 @@
 
 describe BankAccount do
+  subject(:account) { BankAccount.new(100)}
   it "deposits money" do
-    account = BankAccount.new(100)
-    expect(account.deposit(100)).to eq(200)
+    expect(subject.deposit(100)).to eq(200)
   end
 
   it "withdraws money" do
-    account = BankAccount.new(100)
-    expect(account.withdraw(50)).to eq(50)
+    expect(subject.withdraw(50)).to eq(50)
   end
 
   it "returns most up to date balance" do
-    account = BankAccount.new(100)
-    account.deposit(100)
-    account.withdraw(50)
-    expect(account.balance).to eq(150)
+    subject.deposit(100)
+    subject.withdraw(50)
+    expect(subject.balance).to eq(150)
   end
 end
