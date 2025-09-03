19258fd2ddbe454a8a08200157cc50e02ebed9fb
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-03 09:06:55 -0400

    Complete tdd

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,17 @@
-# frozen_string_literal: true
-
 class BankAccount
-  def deposit(amount); end
-  def withdraw(amount); end
-  def balance; end
+  def initialize(initial_balance = 0)
+    @current_balance = initial_balance
+  end
+
+  def deposit(amount)
+    @current_balance += amount
+  end
+
+  def withdraw(amount)
+    @current_balance -= amount
+  end
+
+  def balance
+    @current_balance
+  end
 end
--- spec/student/bank_account_spec.rb
+++ spec/student/bank_account_spec.rb
@@ -1,5 +1,19 @@
-# frozen_string_literal: true
 
 describe BankAccount do
-  # ...student specs...
+  it "deposits money" do
+    account = BankAccount.new(100)
+    expect(account.deposit(100)).to eq(200)
+  end
+
+  it "withdraws money" do
+    account = BankAccount.new(100)
+    expect(account.withdraw(50)).to eq(50)
+  end
+
+  it "returns most up to date balance" do
+    account = BankAccount.new(100)
+    account.deposit(100)
+    account.withdraw(50)
+    expect(account.balance).to eq(150)
+  end
 end
