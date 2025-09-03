eed9cd9f1627c4727b00a0b7710ee7c0ff17b3d1
Mike <mclancy96@gmail.com>
2025-09-03 09:26:51 -0400

    Test bank account methods

--- spec/student/bank_account_spec.rb
+++ spec/student/bank_account_spec.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 describe BankAccount do
-  # ...student specs...
+  it "deposits money" do
+    expect(BankAccount.new.deposit(100)).to eq(100)
+  end
+
+  it "returns a balance" do
+    expect(BankAccount.new(89).balance).to eq(89)
+  end
+
+  it "withdraws money from balance" do
+    expect(BankAccount.new(84_385).withdraw(84_300)).to eq(85)
+  end
 end
