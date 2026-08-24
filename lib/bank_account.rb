01f71c8865558fa07cb1a9e731ed27f128f7d088
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-08-24 11:26:05 -0400

    tdd small ruby class lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 class BankAccount
-  def deposit(amount); end
-  def withdraw(amount); end
-  def balance; end
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def deposit(amount)
+    raise ArgumentError, 'amount must be positive' unless amount.positive?
+
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    raise ArgumentError, 'amount must be positive' unless amount.positive?
+    raise ArgumentError, 'insufficient funds' if amount > @balance
+
+    @balance -= amount
+  end
 end
