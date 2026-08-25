cdf257088a2cd51f04d9dc0f07abab429697785c
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-08-25 09:37:11 -0400

    finsihed lab

--- spec/student/bank_account_spec.rb
+++ spec/student/bank_account_spec.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 describe BankAccount do
-  # ...student specs...
+  it 'increases the balance when money is deposited' do
+    account = BankAccount.new
+    account.deposit(1000)
+    expect(account.balance).to eq (1000)
+  end
+
+  it 'decreases the balance when moneu is withdrawn' do
+    account = BankAccount.new
+    account.deposit(1000)
+    account.withdraw(400)
+    expect(account.balance).to eq (600)
+  end
 end
